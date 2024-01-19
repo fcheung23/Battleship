cb368f948b9330d98f8395036afb3f41fd27230b
fcheung23 <154583304+fcheung23@users.noreply.github.com>
2024-01-19 14:55:16 -0800

    Update main.c

--- main.c
+++ main.c
@@ -7,20 +7,18 @@
 #include "ships.h"
 #include "stages.h"
 
+// BattleShip Game
 int main() {
 
     // for continue commands
     char c;
     int num_args_read = 0;
 
-    // call welcome screen
     print_welcome_screen();
     num_args_read = scanf("%c", &c);
-    // call tutorial screen
     if (num_args_read >= 1) print_tutorial_screen();
     while (getchar() != '\n');
     num_args_read = scanf("%c", &c);
-    // start playing the game
     if (num_args_read >= 1) play_game();
 
 }
